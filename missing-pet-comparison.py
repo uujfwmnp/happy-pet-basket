71c5c10f94c655da760a5c5d711da5a5ecf71fd3
uujfwmnp <3219994+uujfwmnp@users.noreply.github.com>
2020-04-04 23:28:40 -0700

    API change, JSON update, enhanced error handling
    Blizzard changed their API endpoint and also modified the JSON layout. Fixed the script to handle these new changes, and added some extra error handling.

--- missing-pet-comparison.py
+++ missing-pet-comparison.py
@@ -24,21 +24,20 @@ token = json.loads(auth.text)
 
 # Function to request data from Blizzard, and then load the returned text string as JSON list
 def data_request(region,server,character,token):
-    get = requests.get('https://'+region+'.api.blizzard.com/wow/character/'+server+'/'+character+'?fields=pets&access_token='+token['access_token'])
-    if (get.status_code == 401): # Error handling: Invalid access token!
-        exit('Error During Run: ' + get['reason'])
+    get = requests.get('https://'+region+'.api.blizzard.com/profile/wow/character/'+server+'/'+character+'/collections/pets?namespace=profile-'+region+'&locale=en_US&access_token='+token['access_token'])
+    if (get.status_code == 401):   # Error handling: Invalid access token!
+        exit('Error During Run: Invalid Access Token')
+    elif (get.status_code == 404): # Error handling: Invalid character or server!
+        exit('Error Obtaining Data For ' + character + ' ' +server + ': ' + get.json()['detail']+'\n Check the spelling and try again.')
     else:
         data_request = json.loads(get.text)
-    if ('status' in data_request.keys()): # The 'status' key only appears when the character or server is misspelled or missing.
-        exit('Error Obtaining Data For ' + character + ' ' +server + ': ' + data_request['reason'])
-    else:
         return data_request
 
 # Setup empty list, loop through the target array variable, and append Creature Names to the empty list
 def pet_list(target):
     pets = []
     for i in range(0, len(target)):
-        pets.append(target[i]['creatureName'])
+        pets.append(target[i]['species']['name'])
     return pets
 
 # Setting up variables to call the data_request function for each character
@@ -46,8 +45,8 @@ myData = data_request(region,myServer,myCharacter,token)
 targetData = data_request(region,targetServer,targetCharacter,token)
 
 # Calling the pet_list() function to get the collected data for each character
-myPets = pet_list(myData['pets']['collected'])
-targetPets = pet_list(targetData['pets']['collected'])
+myPets = pet_list(myData['pets'])
+targetPets = pet_list(targetData['pets'])
 
 # Result of comparing my pets vs target pets, set as variable, duplicates removed, and sorted alphabetically
 result = list(set(myPets) - set(targetPets))
